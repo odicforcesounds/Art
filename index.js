ec9a946f8a85e05d5ecdf3769d1dd4c09f7d95a1
OscarFM <ofmz@autistici.org>
2022-08-09 13:14:10 +0100

    change way to interact with statistic js script in header.. hope I don't need to add this to each page :D

--- index.js
+++
@@ -1,9 +0,0 @@
-(function () {
-	var s = document.createElement("script");
-	s.src =
-		document.location.protocol + "//static/statvoo.com/js/analytics-min.js";
-	s.type = "text/javascript";
-	s.async = true;
-	var t = document.getElementByTagName("script")[0];
-	t.parentNode.insertBefore(s, t);
-})();
