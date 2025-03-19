7173a87b1f17b2a544eb3b4e8670a732f6df7c51
om <om@localhost>
2025-03-19 17:09:51 +0000

    Make it Right for a better Ride

--- bin/server.sh
+++ bin/server.sh
@@ -2,4 +2,7 @@
 # npm i -g live-server 
 # Author: Monkey 
 
-live-server --host=${MYLOCALIP} --port=${8000}
+MYLOCALIP='ip -4 addr show enp0s25 | grep -oP "(?<=inet ).*(?=/)"'
+PORT='8080'
+
+live-server --host=${MYLOCALIP} --port=${PORT}
