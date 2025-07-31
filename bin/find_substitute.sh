18168dffdaca7d2dcd139f8111d91ae7f3ca1366
om <om@odicforcesounds.com>
2025-07-31 22:46:25 +0000

    Make it Right for a better Ride

--- bin/find_substitute.sh
+++ bin/find_substitute.sh
@@ -1,3 +1,3 @@
 #!/bin/bash  
-# grep -rli 'party' * | xargs -i@ sed -i 's/party/new-party/g' @
+ grep -rli 'G-Q31J8K9QJ1' * | xargs -i@ sed -i 's/G-Q31J8K9QJ1/G-Q31J8K9QJ1/g' @
 # find /home/user/folder1 /home/user/folder2 -type f -name "*.txt" -exec sed -i 's/apple/orange/g' {} +
